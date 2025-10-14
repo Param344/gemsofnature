43bf1b391af4cf07381a11e74a3d49220233d796
Paramjeet Singh <86317957+Param344@users.noreply.github.com>
2025-10-14 13:13:10 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Mobile menu toggle
+const menuToggle = document.querySelector('.navbar .md\\:hidden');
+const navLinks = document.querySelector('.navbar ul');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('hidden');
+  <script src="js/script.js"></script>
+});
